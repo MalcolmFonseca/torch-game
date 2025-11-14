fa9db4af326b36cfd6cf8d77721ca477c8a09bba
MalcolmFonseca <malcolmfonseca@hotmail.com>
2025-11-14 17:30:42 -0500

    all three colors of frog w/ animations

--- scripts/grass-cave/frog.gd
+++ scripts/grass-cave/frog.gd
@@ -1,4 +1,27 @@
 extends Area2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready() -> void:
+	match self.name:
+        #use correct color frog
+		"blue-frog":
+			animated_sprite_2d.play("blue-idle")
+		"red-frog":
+			animated_sprite_2d.play("red-idle")
+		"green-frog":
+			animated_sprite_2d.play("green-idle")
+		_:
+			animated_sprite_2d.play("green-idle")
+
 func _on_body_entered(body: Node2D) -> void:
-    pass
+	match self.name:
+        #call the correct speech bubble
+		"blue-frog":
+			pass
+		"red-frog":
+			pass
+		"green-frog":
+			pass
+		_:
+			pass
