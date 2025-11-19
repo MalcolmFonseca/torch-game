3620c58a819143961ce45c959e1e6cf360c9d183
MalcolmFonseca <malcolmfonseca@hotmail.com>
2025-11-19 16:24:19 -0500

    fixed viewport + text bubble issues, added grass end screen

--- scripts/grass-cave/green_door.gd
+++ scripts/grass-cave/green_door.gd
@@ -1,13 +1,16 @@
 extends Area2D
 
 var is_open: bool = false
+@export_file("*.tscn") var next_scene
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+func _ready():
+    self.body_entered.connect(_on_body_entered)
+
 func _on_body_entered(_body: Node2D) -> void:
     if is_open:
-        #send player to end screen
-        pass
+        get_tree().change_scene_to_file(next_scene)
 
 func open() -> void:
     if (!is_open):
