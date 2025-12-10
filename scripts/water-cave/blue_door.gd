299ce56b7731247c7dcc3001b74555f8a4f93cd8
MalcolmFonseca <malcolmfonseca@hotmail.com>
2025-12-09 21:23:42 -0500

    water end screen song

--- scripts/water-cave/blue_door.gd
+++ scripts/water-cave/blue_door.gd
@@ -10,6 +10,7 @@ func _ready():
 
 func _on_body_entered(_body: Node2D) -> void:
 	if is_open:
+		SoundController.bg_music_play("water_win")
 		get_tree().change_scene_to_file(next_scene)
 
 func open() -> void:
