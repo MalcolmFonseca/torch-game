6a55344e6dc570d100a87532dc644da35e6eab65
Jacob Johnstone <jjohn483@uwo.ca>
2025-12-14 16:09:24 -0500

    Lava visual change, adjusted lava sfx and music to (hopefully) equal volumes

--- scripts/lava-cave/fire_door.gd
+++ scripts/lava-cave/fire_door.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func open_door():
 	animated_sprite_2d.play("Opening")
+	#audio_stream_player_2d.play()
 
 func _on_body_entered(body: Node2D) -> void:
 	if(body.name == "Player"):
--- scripts/lava-cave/salamander.gd
+++ scripts/lava-cave/salamander.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
 var text_bubble_asset: PackedScene = preload("res://scenes/text_bubble.tscn")
 var text_bubble: Node = null
 
@@ -10,9 +12,10 @@ func _on_body_entered(body: Node2D) -> void:
 	if (!text_bubble && body.name == "Player"):
 		text_bubble = text_bubble_asset.instantiate()
 		text_bubble.messageText = "A torch's glow brightens my day"
+		audio_stream_player_2d.play()
 		add_child(text_bubble)
 
-func _on_body_exited(body: Node2D) -> void:
+func _on_body_exited(_body: Node2D) -> void:
 	if (text_bubble):
 		text_bubble.queue_free()
 	text_bubble = null
